a12334cd7ca11b06b107a3bb7cd0f754cde1961a
Yura <114563784+YuraKravchuk@users.noreply.github.com>
2023-08-19 16:24:27 +0300

    ц

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const renderList = (arr, container) => {
+  const markup = arr
+    .map(
+      (item) =>
+        `<li class="gallery_item">
+        <a class="gallery_link" href="${item.original}" onclick="return false;">
+            <img
+            class="gallery_image"
+            src="${item.preview}" 
+            data-source="${item.original}" 
+            alt="${item.description}"/>
+        </a>
+    </li>`
+    )
+    .join("");
+  container.insertAdjacentHTML("beforeend", markup);
+};
+const handListClick = (e) => {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const modal = basicLightbox.create(
+    `<div class="modal"><img src="${e.target.dataset.source}"/></div>`
+  );
+  modal.show();
+};
+renderList(galleryItems, gallery);
+gallery.addEventListener("click", handListClick);
+// console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const renderList = (arr, container) => {
+  const markup = arr
+    .map(
+      (item) =>
+        `<li class="gallery_item">
+        <a class="gallery_link" href="${item.original}" onclick="return false;">
+            <img
+            class="gallery_image"
+            src="${item.preview}" 
+            data-source="${item.original}" 
+            alt="${item.description}"/>
+        </a>
+    </li>`
+    )
+    .join("");
+  container.insertAdjacentHTML("beforeend", markup);
+};
+
+// console.log(galleryItems);
+renderList(galleryItems, gallery);
